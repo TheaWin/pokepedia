ba70dbe75040b12a4fa9de377124fec5c693ce61
Thea Win <winwinkhaing.wk.14@gmail.com>
2023-11-23 23:36:12 +0800

    pokemonList array
    added 3 pokemon objects to pokemonList array

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+let pokemonList = [
+    {
+        id: '0001',
+        name: 'Bulbasaur',
+        height: 2,
+        types: ['grass', 'poison']
+    },
+    {
+        id: '0002',
+        name: 'Ivysaur',
+        height: 3,
+        types: ['grass', 'poison']
+    },
+    {
+        id: '0003',
+        name: 'Venusaur',
+        height: 6,
+        types: ['grass', 'poison']
+    }
+];
