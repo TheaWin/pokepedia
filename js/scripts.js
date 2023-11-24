1b97113d53408c98868b26389c0fd4e6ccb19dab
Thea Win <winwinkhaing.wk.14@gmail.com>
2023-11-24 21:05:33 +0800

    1.3 Task
    added for loop as well as adding condition in the loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// set of pokemon arrays
 let pokemonList = [
     {
         id: '0001',
@@ -18,3 +19,16 @@ let pokemonList = [
         types: ['grass', 'poison']
     }
 ];
+
+/* line 29: for loop to iterates over the objects in the arra
+ line 30 & 31: declaring object name to look for specific values
+line 31 & 32: using template literal to show result 
+line 32: as well as adding conition within the loop
+ */
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokeName = pokemonList[i].name;
+    let pokeHeight = pokemonList[i].height;
+    document.write(`<p>${pokeName} 
+      ${pokeHeight >= 4 ? `(height: ${pokeHeight} - Wow, that's big!` : `(height: ${pokeHeight}`})</p>`);
+    
+  }
