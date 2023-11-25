c3822f05e939f897258888ef6b787ed8883abdcb
Thea Win <winwinkhaing.wk.14@gmail.com>
2023-11-25 20:41:08 +0800

    wrapped pokemonList in IIFE
    with add and getAll function

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,40 @@
-// set of pokemon arrays
-let pokemonList = [
-    {
-        id: '0001',
-        name: 'Bulbasaur',
-        height: 2,
-        types: ['grass', 'poison']
-    },
-    {
-        id: '0002',
-        name: 'Ivysaur',
-        height: 3,
-        types: ['grass', 'poison']
-    },
-    {
-        id: '0003',
-        name: 'Venusaur',
-        height: 6,
-        types: ['grass', 'poison']
-    }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            id: '0001',
+            name: 'Bulbasaur',
+            height: 2,
+            types: ['grass', 'poison']
+        },
+        {
+            id: '0002',
+            name: 'Ivysaur',
+            height: 3,
+            types: ['grass', 'poison']
+        },
+        {
+            id: '0003',
+            name: 'Venusaur',
+            height: 6,
+            types: ['grass', 'poison']
+        }
+    ];
 
-pokemonList.forEach (function(poke) {
-    if (poke.height >=4) {
-        document.write (`<p>${poke.name} - (height: ${poke.height} - Wow, that's big!)</p>`);
+    return {
+        getAll: function () {
+            return pokemonList;
+        },
+        add: function (pokemon) {
+            pokemonList.push(pokemon);
+        }
+    };
+})();
+
+pokemonRepository.getAll().forEach(function(poke){
+    if (poke.height >= 4) {
+        document.write(`<p>${poke.name} - (height: ${poke.height} - Wow, that's big!)</p>`);
     } else {
-        document.write (`<p>${poke.name} - (height: ${poke.height})</p>`);
+        document.write(`<p>${poke.name} - (height: ${poke.height})</p>`);
     }
-    }
-)
+}
+);
