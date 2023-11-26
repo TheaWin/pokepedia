5dbf88f3c9799e5c5f92017de1f1dfa05b601ee5
Thea Win <winwinkhaing.wk.14@gmail.com>
2023-11-26 13:23:48 +0800

    DOM Manipulation
    added buttons with each pokemon in the array and styled the html

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,50 @@
-let pokemonRepository = (function () {
+let pokemonRepository = (function() {
     let pokemonList = [
-        {
-            id: '0001',
-            name: 'Bulbasaur',
-            height: 2,
-            types: ['grass', 'poison']
-        },
-        {
-            id: '0002',
-            name: 'Ivysaur',
-            height: 3,
-            types: ['grass', 'poison']
-        },
-        {
-            id: '0003',
-            name: 'Venusaur',
-            height: 6,
-            types: ['grass', 'poison']
-        }
+      {
+        id: '0001',
+        name: 'Bulbasaur',
+        height: 2,
+        types: ['grass', 'poison']
+      },
+      {
+        id: '0002',
+        name: 'Ivysaur',
+        height: 3,
+        types: ['grass', 'poison']
+      },
+      {
+        id: '0003',
+        name: 'Venusaur',
+        height: 6,
+        types: ['grass', 'poison']
+      }
     ];
-
+  
     return {
-        getAll: function () {
-            return pokemonList;
-        },
-        // add conditional to add in only the correct type
-        add: function (pokemon) {
-            if (typeof pokemon === 'object' && "name" in pokemon && "height" in pokemon) {
-                pokemonList.push(pokemon);
-            } else {
-                console.log("Pokemon is not correct");
-            }
+      getAll: function() {
+        return pokemonList;
+      },
+      // add conditional to add in only the correct type
+      add: function(pokemon) {
+        if (typeof pokemon === 'object' && "name" in pokemon && "height" in pokemon) {
+          pokemonList.push(pokemon);
+        } else {
+          console.log("Pokemon is not correct");
         }
+      },
+      //addListItem for DOM Manipulation
+      addListItem: function(pokemon) {
+        let pokeList = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('button');
+        listItem.appendChild(button);
+        pokeList.appendChild(listItem);
+      }
     };
-})();
-
-pokemonRepository.getAll().forEach(function (poke) {
-    if (poke.height >= 4) {
-        document.write(`<p>${poke.name} - (height: ${poke.height} - Wow, that's big!)</p>`);
-    } else {
-        document.write(`<p>${poke.name} - (height: ${poke.height})</p>`);
-    }
-}
-);
+  })();
+  
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
